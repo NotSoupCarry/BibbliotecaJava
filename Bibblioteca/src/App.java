693326c8f49510f52b309bf130413add608e35c5
Giuseppe Conza <giuseppeforce36@gmail.com>
2025-03-05 15:23:34 +0100

    general implementation

--- Bibblioteca/src/App.java
+++ Bibblioteca/src/App.java
@@ -1,10 +1,15 @@
 import java.util.Date;
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.util.Scanner;
+import java.sql.*;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Biblioteca biblioteca = new Biblioteca();
+
+        Menu.menuPrincipale(scanner, biblioteca);
+
+        scanner.close();
     }
 }
 
@@ -97,14 +102,185 @@ class Libro {
 
 // classe con metodi gestione db
 class Biblioteca {
+    private Connection conn;
+
+    public Biblioteca() {
+        this.conn = DBContext.connessioneDatabase();
+    }
+
+    // Metodo per stampare tutti i libri
+    public void stampaTuttiLibri() {
+        if (conn != null) {
+            String query = "SELECT * FROM libri";
+            try (PreparedStatement stmt = conn.prepareStatement(query)) {
+                ResultSet rs = stmt.executeQuery();
+
+                System.out.println("\n === Elenco di tutti i libri: ===\n");
+                while (rs.next()) {
+                    String nome = rs.getString("nome");
+                    int quantitaTotale = rs.getInt("quantita_totale");
+                    int quantitaPrestati = rs.getInt("quantita_prestati");
+                    Date dataPubblicazione = rs.getDate("data_pubblicazione");
+
+                    int disponibili = quantitaTotale - quantitaPrestati;
+                    // Stampa i dettagli del libro
+                    System.out.println(nome + " | " + dataPubblicazione + ", Copie Totali: " + quantitaTotale +
+                            ", Prestati: " + quantitaPrestati + ", Disponibili: " + disponibili);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // Metodo per prestare un libro
+    public void prestaLibro(String nomeLibro) {
+        if (conn != null) {
+            // Ricerca il libro per nome
+            String query = "SELECT ID, quantita_totale, quantita_prestati FROM libri WHERE nome = ?";
+            try (PreparedStatement stmt = conn.prepareStatement(query)) {
+                stmt.setString(1, nomeLibro);
+                ResultSet rs = stmt.executeQuery();
+
+                if (rs.next()) {
+                    int libroId = rs.getInt("ID");
+                    int quantitaTotale = rs.getInt("quantita_totale");
+                    int quantitaPrestati = rs.getInt("quantita_prestati");
+
+                    // Verifica se ci sono libri disponibili per il prestito
+                    if (quantitaTotale > quantitaPrestati) {
+                        // Se sì, aumenta i libri prestati
+                        String updateQuery = "UPDATE libri SET quantita_prestati = quantita_prestati + 1 WHERE ID = ?";
+                        try (PreparedStatement updateStmt = conn.prepareStatement(updateQuery)) {
+                            updateStmt.setInt(1, libroId);
+                            updateStmt.executeUpdate();
+                            System.out.println("Libro prestato con successo!");
+                        }
+                    } else {
+                        System.out.println("Non ci sono copie disponibili da prestare per il libro selezionato");
+                    }
+                } else {
+                    System.out.println("Libro non trovato.");
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // Metodo per restituire un libro
+    public void restituisciLibro(String nomeLibro) {
+        if (conn != null) {
+            // Ricerca il libro per nome
+            String query = "SELECT ID, quantita_totale, quantita_prestati FROM libri WHERE nome = ?";
+            try (PreparedStatement stmt = conn.prepareStatement(query)) {
+                stmt.setString(1, nomeLibro);
+                ResultSet rs = stmt.executeQuery();
+
+                if (rs.next()) {
+                    int libroId = rs.getInt("ID");
+                    int quantitaPrestati = rs.getInt("quantita_prestati");
+
+                    // Verifica se ci sono libri prestati da restituire
+                    if (quantitaPrestati > 0) {
+                        // Se sì, diminuisce il numero di libri prestati
+                        String updateQuery = "UPDATE libri SET quantita_prestati = quantita_prestati - 1 WHERE ID = ?";
+                        try (PreparedStatement updateStmt = conn.prepareStatement(updateQuery)) {
+                            updateStmt.setInt(1, libroId);
+                            updateStmt.executeUpdate();
+                            System.out.println("Libro restituito con successo!");
+                        }
+                    } else {
+                        System.out.println("Non ci sono copie disponibili da restituire per il libro selezionato");
+                    }
+                } else {
+                    System.out.println("Libro non trovato.");
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
 
 // classe dei menu
-class Menu{
+class Menu {
+    // menu principale
+    public static void menuPrincipale(Scanner scanner, Biblioteca biblioteca) {
+        int scelta;
+        boolean exitMainMenu = false;
+        System.out.println("\n==== Benvenuto in BIBBLIOTECA INSANA ====");
 
+        while (!exitMainMenu) {
+            System.out.println("\n==== Menu ====");
+            System.out.println("1. Mostra tutti i Libri");
+            System.out.println("2. Presta Libro");
+            System.out.println("3. Restituisci Libro");
+            System.out.println("4. Aggiungi Libro");
+            System.out.println("5. Elimina Libro");
+            System.out.println("6. Esci");
+
+            System.out.print("Scegli un'opzione (1-4): ");
+            scelta = Controlli.controlloInputInteri(scanner);
+            scanner.nextLine();
+
+            switch (scelta) {
+                case 1:
+                    biblioteca.stampaTuttiLibri();
+                    break;
+                case 2:
+                    System.out.print("Inserisci il nome del libro da prestare: ");
+                    String nomeLibroDaPrestare = Controlli.controlloInputStringhe(scanner);
+                    biblioteca.prestaLibro(nomeLibroDaPrestare);
+                    break;
+                case 3:
+                    System.out.print("Inserisci il nome del libro da restituire: ");
+                    String nomeLibroDaRestituire = Controlli.controlloInputStringhe(scanner);
+                    biblioteca.restituisciLibro(nomeLibroDaRestituire);
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    System.out.println("Uscita dal programma.");
+                    exitMainMenu = true;
+                    break;
+                default:
+                    System.out.println("Opzione non valida! Riprova.");
+            }
+        }
+    }
 }
 
 // classe dei controlli
-class Controlli{
+class Controlli {
+    // Metodo per controllare che l'input stringa non sia vuoto
+    public static String controlloInputStringhe(Scanner scanner) {
+        String valore;
+        do {
+            valore = scanner.nextLine().trim();
+            if (valore.isEmpty()) {
+                System.out.print("Input non valido. Inserisci un testo: ");
+            }
+        } while (valore.isEmpty());
+        return valore;
+    }
 
+    // Metodo per controllare l'input intero
+    public static Integer controlloInputInteri(Scanner scanner) {
+        Integer valore;
+        do {
+            while (!scanner.hasNextInt()) {
+                System.out.print("Devi inserire un numero intero. Riprova ");
+                scanner.next();
+            }
+            valore = scanner.nextInt();
+            if (valore < 0) {
+                System.out.print("Il numero non può essere negativo. Riprova: ");
+            }
+        } while (valore < 0);
+        return valore;
+    }
 }
